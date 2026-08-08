dcc880c60022aefa88fbe06a417e74f1dc512d77
Gythrie <151329547+Gythrie@users.noreply.github.com>
2026-08-09 00:12:33 +0530

    Refactor websocket endpoint and improve readability
    Refactor websocket handling and improve code formatting.

--- app.py
+++ app.py
@@ -1,24 +1,40 @@
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi import FastAPI
 from fastapi.templating import Jinja2Templates
 from fastapi.requests import Request
+from fastapi.websockets import WebSocket, WebSocketDisconnect
 from manager import WebSocketManager
 
 app = FastAPI()
 
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(
+    directory="templates"
+)
+
 manager = WebSocketManager()
 
-@app.get("/")
+@app.get('/')
 async def root(request: Request):
-    return templates.TemplateResponse(request, "index.html",{})
+    return templates.TemplateResponse(
+        request,
+        'index.html',
+        {}
+    )
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await manager.connect(websocket)
-    try:
-        while True:
+
+    while True:
+        try:
             message = await websocket.receive_json()
-            print(f"Received message: {message}")
-            await manager.broadcast(websocket, message)
-    except WebSocketDisconnect:
-        await manager.disconnect(websocket)
+
+            
+            for client in manager.connected_clients:
+                await manager.send_message(client, message)
+
+
+        except WebSocketDisconnect:
+            await manager.disconnect(websocket)
+        
+
+    
